7b75343ee3f6284453588542b3b6a1d7cf7c1816
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:52:28 +0100

    cacheLRU

---
+++ cacheLRU.js
@@ -0,0 +1,10 @@
+/* 📘 Énoncé 
+Implémenter une structure LRUCache(capacity).
+ Methodes : 
+get(key)
+put(key, value)
+Contraintes
+    •	get et put en O(1)
+    •	Suppression du moins récemment utilisé
+    •	Pas de librairie externe
+ */
