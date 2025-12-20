5776e986701e624594cc2ed04a1dc4adabdff476
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:59:50 +0100

    courtChemin-answer

--- courtChemin.js
+++ courtChemin.js
@@ -13,6 +13,50 @@ Implémenter shortestPath(graph, start, end) qui retourne :
 
 */
 
+function shortestPath(graph, start, end) {
+    const distances = {};
+    const prev = {};
+    const visited = new Set();
+
+    Object.keys(graph).forEach(n => distances[n] = Infinity);
+    distances[start] = 0;
+
+    while (true) {
+        let node = null;
+        let min = Infinity;
+
+        for (const n in distances) {
+            if (!visited.has(n) && distances[n] < min) {
+                min = distances[n];
+                node = n;
+            }
+        }
+
+        if (node === null) break;
+        if (node === end) break;
+
+        visited.add(node);
+
+        for (const neigh in graph[node]) {
+            const dist = distances[node] + graph[node][neigh];
+            if (dist < distances[neigh]) {
+                distances[neigh] = dist;
+                prev[neigh] = node;
+            }
+        }
+    }
+
+    const path = [];
+    let cur = end;
+    while (cur) {
+        path.unshift(cur);
+        cur = prev[cur];
+    }
+
+    return { distance: distances[end], path };
+}
+
+
 const graph = {
     A: { B: 5, C: 2 },
     B: { A: 5, D: 1 },
