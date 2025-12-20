68afd0d2f456471b4a8276a5bb137aec322b5c97
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:54:56 +0100

    cacheLRU-answer

--- cacheLRU.js
+++ cacheLRU.js
@@ -9,9 +9,33 @@ Contraintes
     •	Pas de librairie externe
  */
 
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.map = new Map();
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1;
+
+        const value = this.map.get(key);
+        this.map.delete(key);
+        this.map.set(key, value);
+        return value;
+    }
+
+    put(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key);
+        } else if (this.map.size === this.capacity) {
+            const lruKey = this.map.keys().next().value;
+            this.map.delete(lruKey);
+        }
+        this.map.set(key, value);
+    }
+}
 
 const cache = new LRUCache(2);
-
 cache.put(1, "A");
 cache.put(2, "B");
 cache.get(1);      // "A"
