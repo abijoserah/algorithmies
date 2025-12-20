a56e15f188d65e0b6b569cb421964c2386dbc54d
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:44:06 +0100

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,8 @@
+/*📘 Énoncé
+On te donne un entier n (0 ≤ n ≤ 1_000_000).
+Travail à faire
+Écrire fib(n) qui retourne le nᵉ Fibonacci exact.
+Contraintes
+    •	Temps < 1 seconde
+    •	Pas de récursion naïve
+    •	Résultat exact (BigInt)*/
