378c713a8da7a0803c7b8a32e55a4fd47db317e2
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:58:42 +0100

    courtChemin

---
+++ courtChemin.js
@@ -0,0 +1,14 @@
+/* 📘 Exercice / Instructions
+On te donne un graphe pondéré non orienté sous forme d’objet.
+Objectif
+Implémenter shortestPath(graph, start, end) qui retourne : 
+{
+  distance: number,
+  path: string[]
+}
+  Contraintes
+    •	Graphes jusqu’à 10 000 nœuds
+    •	Pas de brute force
+    •	Doit utiliser un algo optimal
+
+*/
