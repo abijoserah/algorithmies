500233adff308595bacfb489ba1e4adf05fef5e0
abijoserah <serahabijo94@gmail.com>
2025-12-20 21:36:13 +0100

    rfcq

---
+++ RailFenceCiphar.js
@@ -0,0 +1,13 @@
+/*📘 Énoncé
+On te donne :
+    •	une chaîne text
+    •	un entier rails ≥ 2
+Le texte est écrit en zigzag sur rails lignes, puis lu ligne par ligne.
+Travail à faire
+    1.	Implémenter encode(text, rails)
+    2.	Implémenter decode(text, rails)
+Contraintes
+    •	Complexité O(n)
+    •	Pas de matrice 2D complète
+    •	Les espaces comptent comme caractères*/
+
