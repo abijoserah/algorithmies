ac489f05f727032a526a7aa082af78779ef9ca62
abijoserah <serahabijo94@gmail.com>
2025-12-20 22:02:01 +0100

    parser-answer

--- parser.js
+++ parser.js
@@ -8,6 +8,25 @@ Règles
     •	Pas de eval
  */
 
+function isValidExpression(expr) {
+    const stack = [];
+    const operators = "+-*/";
+
+    let prev = "";
+
+    for (const c of expr.replace(/\s/g, "")) {
+        if (c === "(") stack.push(c);
+        else if (c === ")") {
+            if (!stack.pop()) return false;
+        }
+
+        if (operators.includes(c) && operators.includes(prev)) return false;
+        prev = c;
+    }
+
+    return stack.length === 0 && !operators.includes(prev);
+}
+
 console.assert(isValidExpression("(2+3)*5") === true);
 console.assert(isValidExpression("2+*3") === false);
 console.assert(isValidExpression("((3+5)") === false);
