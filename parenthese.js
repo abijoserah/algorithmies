3db20188ae7536d9e00f58641b1a9f7bfe2a336e
abijoserah <serahabijo94@gmail.com>
2025-12-20 22:04:37 +0100

    parenthese-answer

--- parenthese.js
+++ parenthese.js
@@ -3,6 +3,21 @@ On te donne une chaîne contenant { } [ ] ( ).
 Objectif
 Implémenter isBalanced(str).*/
 
+function isBalanced(str) {
+    const map = { ")": "(", "]": "[", "}": "{" };
+    const stack = [];
+
+    for (const c of str) {
+        if (map[c]) {
+            if (stack.pop() !== map[c]) return false;
+        } else {
+            stack.push(c);
+        }
+    }
+
+    return stack.length === 0;
+}
+
 console.assert(isBalanced("{[()]}") === true);
 console.assert(isBalanced("{[(])}") === false);
 console.assert(isBalanced("((()))") === true);
