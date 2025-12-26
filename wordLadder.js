b0e0b5d2f5fbe8a531b9cf1bf1b1630192309974
abijoserah <serahabijo94@gmail.com>
2025-12-26 18:14:35 +0100

    wordLadder-test

--- wordLadder.js
+++ wordLadder.js
@@ -1,3 +1,11 @@
 /* On te donne beginWord, endWord et une liste wordList.
 Objectif
 Implémenter ladderLength(beginWord, endWord, wordList) qui retourne la longueur de la transformation la plus courte. */
+
+
+console.assert(
+    ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]) === 5
+);
+console.assert(
+    ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log"]) === 0
+);
