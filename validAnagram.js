5cd07c08d4c763b7990e2233ca2c1d2005d5918f
abijoserah <serahabijo94@gmail.com>
2025-12-26 18:12:57 +0100

    Anagrams-test

--- validAnagram.js
+++ validAnagram.js
@@ -1,3 +1,8 @@
 /* On te donne deux chaînes a et b.
 Objectif
-Implémenter isAnagram(a, b) qui retourne true si ce sont des anagrammes. */ 
+Implémenter isAnagram(a, b) qui retourne true si ce sont des anagrammes. */
+
+
+console.assert(isAnagram("listen", "silent") === true);
+console.assert(isAnagram("anagram", "nagaram") === true);
+console.assert(isAnagram("rat", "car") === false);
