a7b85e2abcccec0155cf5910292bc23d44d12cd1
abijoserah <serahabijo94@gmail.com>
2025-12-26 18:14:50 +0100

    wordLadder-answer

--- wordLadder.js
+++ wordLadder.js
@@ -2,6 +2,33 @@
 Objectif
 Implémenter ladderLength(beginWord, endWord, wordList) qui retourne la longueur de la transformation la plus courte. */
 
+function ladderLength(beginWord, endWord, wordList) {
+    const set = new Set(wordList);
+    if (!set.has(endWord)) return 0;
+
+    let queue = [[beginWord, 1]];
+
+    while (queue.length) {
+        const [word, len] = queue.shift();
+        if (word === endWord) return len;
+
+        for (let i = 0; i < word.length; i++) {
+            for (let c = 97; c <= 122; c++) {
+                const next =
+                    word.slice(0, i) +
+                    String.fromCharCode(c) +
+                    word.slice(i + 1);
+
+                if (set.has(next)) {
+                    set.delete(next);
+                    queue.push([next, len + 1]);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
 
 console.assert(
     ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]) === 5
