914cb109a0cbba1f0fdee2c718cc42edebe9018c
abijoserah <serahabijo94@gmail.com>
2025-12-27 20:31:51 +0100

    decodeString-test

--- decodeString.js
+++ decodeString.js
@@ -1,3 +1,8 @@
 /* On te donne une chaîne encodée comme 3[a2[c]].
 Objectif
 Implémenter decodeString(s) qui retourne la chaîne décodée. */
+
+
+console.assert(decodeString("3[a]2[bc]") === "aaabcbc");
+console.assert(decodeString("3[a2[c]]") === "accaccacc");
+console.assert(decodeString("2[abc]3[cd]ef") === "abcabccdcdcdef");
