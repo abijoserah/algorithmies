24f70ca1331f1518ba85865b48b6bbd41539a983
abijoserah <serahabijo94@gmail.com>
2025-12-27 20:32:33 +0100

    decodeString-answer

--- decodeString.js
+++ decodeString.js
@@ -2,6 +2,28 @@
 Objectif
 Implémenter decodeString(s) qui retourne la chaîne décodée. */
 
+function decodeString(s) {
+    const stack = [];
+    let num = 0;
+    let str = "";
+
+    for (const c of s) {
+        if (!isNaN(c)) {
+            num = num * 10 + Number(c);
+        } else if (c === "[") {
+            stack.push([str, num]);
+            str = "";
+            num = 0;
+        } else if (c === "]") {
+            const [prev, n] = stack.pop();
+            str = prev + str.repeat(n);
+        } else {
+            str += c;
+        }
+    }
+
+    return str;
+}
 
 console.assert(decodeString("3[a]2[bc]") === "aaabcbc");
 console.assert(decodeString("3[a2[c]]") === "accaccacc");
