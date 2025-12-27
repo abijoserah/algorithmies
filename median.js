4edc313e6a23d2c6f5e0fe2d944a52875a8fe54f
abijoserah <serahabijo94@gmail.com>
2025-12-27 20:30:12 +0100

    median-test

--- median.js
+++ median.js
@@ -1,3 +1,8 @@
 /* On te donne deux tableaux triés nums1 et nums2.
 Objectif
 Implémenter findMedianSortedArrays(nums1, nums2) en O(log(n+m)). */
+
+
+console.assert(findMedianSortedArrays([1, 3], [2]) === 2);
+console.assert(findMedianSortedArrays([1, 2], [3, 4]) === 2.5);
+console.assert(findMedianSortedArrays([0, 0], [0, 0]) === 0);
