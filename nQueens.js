0d51e65f19b302a7bcc4894efbb0d5aea9dd232e
abijoserah <serahabijo94@gmail.com>
2026-01-10 19:04:55 +0100

    n queens test

--- nQueens.js
+++ nQueens.js
@@ -1,3 +1,7 @@
 /*On te donne un entier n.
     Objectif
 Implémenter solveNQueens(n) qui retourne toutes les solutions, chaque solution étant un tableau de strings(Q et.).*/
+
+console.assert(solveNQueens(1).length === 1);
+console.assert(solveNQueens(4).length === 2);
+console.assert(solveNQueens(5).length === 10);
