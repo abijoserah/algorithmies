fb369ae691106d77832c3f3c0967276b1b5d97a9
abijoserah <serahabijo94@gmail.com>
2026-01-10 19:04:44 +0100

    n queens

--- nQueens.js
+++ nQueens.js
@@ -1,3 +1,3 @@
-On te donne un entier n.
+/*On te donne un entier n.
     Objectif
-Implémenter solveNQueens(n) qui retourne toutes les solutions, chaque solution étant un tableau de strings(Q et.).
+Implémenter solveNQueens(n) qui retourne toutes les solutions, chaque solution étant un tableau de strings(Q et.).*/
