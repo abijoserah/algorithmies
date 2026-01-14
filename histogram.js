ddbb9315cfbcb089b97b84af8db07c77806b551b
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:02:07 +0100

    histogram-answer

--- histogram.js
+++ histogram.js
@@ -2,6 +2,25 @@
 Objectif
 Implémenter largestRectangleArea(heights) qui retourne l’aire max d’un rectangle dans l’histogramme. */
 
+function largestRectangleArea(heights) {
+    const stack = [];
+    let best = 0;
+
+    for (let i = 0; i <= heights.length; i++) {
+        const h = i === heights.length ? 0 : heights[i];
+        while (stack.length && h < heights[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            const height = heights[top];
+            const left = stack.length ? stack[stack.length - 1] : -1;
+            const width = i - left - 1;
+            best = Math.max(best, height * width);
+        }
+        stack.push(i);
+    }
+
+    return best;
+}
+
 console.assert(largestRectangleArea([2, 1, 5, 6, 2, 3]) === 10);
 console.assert(largestRectangleArea([2, 4]) === 4);
 console.assert(largestRectangleArea([1, 1]) === 2);
