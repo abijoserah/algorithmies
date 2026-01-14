bc6c4f73dea3f6a2981131cdd40fb2d780370f47
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:09:13 +0100

    Product of array except self answer

--- arrayExceptSelf.js
+++ arrayExceptSelf.js
@@ -2,6 +2,23 @@
 Objectif
 Implémenter productExceptSelf(nums) qui retourne un tableau où res[i] = produit de tous les éléments sauf nums[i], sans utiliser la division. */
 
+function productExceptSelf(nums) {
+    const res = Array(nums.length).fill(1);
+
+    let p = 1;
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = p;
+        p *= nums[i];
+    }
+
+    p = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= p;
+        p *= nums[i];
+    }
+
+    return res;
+}
 
 console.assert(JSON.stringify(productExceptSelf([1, 2, 3, 4])) === JSON.stringify([24, 12, 8, 6]));
 console.assert(JSON.stringify(productExceptSelf([-1, 1, 0, -3, 3])) === JSON.stringify([0, 0, 9, 0, 0]));
