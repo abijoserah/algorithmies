c71420b5154ca8f45c8fba9ac5938954211fc884
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:08:59 +0100

    Product of array except self test

--- arrayExceptSelf.js
+++ arrayExceptSelf.js
@@ -1,3 +1,7 @@
 /* On te donne un tableau nums.
 Objectif
 Implémenter productExceptSelf(nums) qui retourne un tableau où res[i] = produit de tous les éléments sauf nums[i], sans utiliser la division. */
+
+
+console.assert(JSON.stringify(productExceptSelf([1, 2, 3, 4])) === JSON.stringify([24, 12, 8, 6]));
+console.assert(JSON.stringify(productExceptSelf([-1, 1, 0, -3, 3])) === JSON.stringify([0, 0, 9, 0, 0]));
