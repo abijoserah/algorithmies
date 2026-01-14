9323680df5bfd7bbae39148598ca93715019996a
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:01:53 +0100

    histogram-test

--- histogram.js
+++ histogram.js
@@ -1,3 +1,7 @@
 /* On te donne un tableau heights.
 Objectif
 Implémenter largestRectangleArea(heights) qui retourne l’aire max d’un rectangle dans l’histogramme. */
+
+console.assert(largestRectangleArea([2, 1, 5, 6, 2, 3]) === 10);
+console.assert(largestRectangleArea([2, 4]) === 4);
+console.assert(largestRectangleArea([1, 1]) === 2);
