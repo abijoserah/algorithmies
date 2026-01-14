42a31f68bf4e2a4ae8aa005683f981160b9aebfa
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:04:05 +0100

    word-search test

--- wordSearch.js
+++ wordSearch.js
@@ -1,3 +1,14 @@
 /*On te donne une grille board de lettres et un tableau words.
 Objectif
 Implémenter findWords(board, words) qui retourne tous les mots trouvables en bougeant haut/bas/gauche/droite sans réutiliser une case.*/
+
+
+const b1 = [
+    ["o", "a", "a", "n"],
+    ["e", "t", "a", "e"],
+    ["i", "h", "k", "r"],
+    ["i", "f", "l", "v"]
+];
+const w1 = ["oath", "pea", "eat", "rain"];
+const out1 = findWords(b1, w1).sort();
+console.assert(JSON.stringify(out1) === JSON.stringify(["eat", "oath"]));
