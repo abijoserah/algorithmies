4ff80ff84dcb8bd003aa72273ee45a1a64aef446
abijoserah <serahabijo94@gmail.com>
2026-01-14 13:04:21 +0100

    word-search answer

--- wordSearch.js
+++ wordSearch.js
@@ -2,6 +2,46 @@
 Objectif
 Implémenter findWords(board, words) qui retourne tous les mots trouvables en bougeant haut/bas/gauche/droite sans réutiliser une case.*/
 
+function findWords(board, words) {
+    const root = {};
+    for (const w of words) {
+        let node = root;
+        for (const ch of w) node = node[ch] ??= {};
+        node.$ = w;
+    }
+
+    const res = [];
+    const R = board.length;
+    const C = board[0].length;
+
+    const dfs = (r, c, node) => {
+        const ch = board[r][c];
+        const next = node[ch];
+        if (!next) return;
+
+        if (next.$) {
+            res.push(next.$);
+            next.$ = null;
+        }
+
+        board[r][c] = "#";
+
+        if (r > 0 && board[r - 1][c] !== "#") dfs(r - 1, c, next);
+        if (c > 0 && board[r][c - 1] !== "#") dfs(r, c - 1, next);
+        if (r + 1 < R && board[r + 1][c] !== "#") dfs(r + 1, c, next);
+        if (c + 1 < C && board[r][c + 1] !== "#") dfs(r, c + 1, next);
+
+        board[r][c] = ch;
+
+        if (Object.keys(next).length === 0) delete node[ch];
+    };
+
+    for (let r = 0; r < R; r++) {
+        for (let c = 0; c < C; c++) dfs(r, c, root);
+    }
+
+    return res;
+}
 
 const b1 = [
     ["o", "a", "a", "n"],
